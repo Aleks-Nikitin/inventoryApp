dc9231ea26f416a7df7431e721d0c223755735d6
Power366 <172065612+Power366@users.noreply.github.com>
2026-02-06 13:19:38 -0600

    deploy

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
-app.listen(process.env.PORT||3000,"0.0.0.0",(err)=>{
+const port = process.env.PORT || 8070;
+app.listen(port,"0.0.0.0",(err)=>{
     if(err){
-        throw err
+        throw new Error
         
     }
     console.log("server started");
--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const {Pool}=require("pg");
 require("dotenv").config();
 
 module.exports=new Pool({
-    connectionString:process.env.DB_CON||process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL
 })
